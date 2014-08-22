80dd0c34d4560f56c3f82a1615f84198da0937f0
sh1r0 <sh1r0@users.noreply.github.com>
2014-08-22 22:20:31 +0800

    added submodule caffe (mobile)

--- caffe-mobile/jni/Android.mk
+++ caffe-mobile/jni/Android.mk
@@ -11,68 +11,64 @@ include $(PREBUILT_STATIC_LIBRARY)
 
 #-----------------------------------------------------------------------------
 include $(CLEAR_VARS)
+include $(LOCAL_PATH)/../../opencv/jni/OpenCV.mk
 LOCAL_MODULE := libcaffe
 # LOCAL_MODULE_TAGS := optional
 
-CXX_SRC :=  \
-src/caffe/syncedmem.cpp \
-src/caffe/util/upgrade_proto.cpp \
-src/caffe/util/math_functions.cpp \
-src/caffe/util/insert_splits.cpp \
-src/caffe/util/im2col.cpp \
-src/caffe/util/io.cpp \
-src/caffe/blob.cpp \
-src/caffe/net.cpp \
-src/caffe/layers/pooling_layer.cpp \
-src/caffe/layers/argmax_layer.cpp \
-src/caffe/layers/relu_layer.cpp \
-src/caffe/layers/split_layer.cpp \
-src/caffe/layers/euclidean_loss_layer.cpp \
-src/caffe/layers/power_layer.cpp \
-src/caffe/layers/slice_layer.cpp \
-src/caffe/layers/threshold_layer.cpp \
-src/caffe/layers/dropout_layer.cpp \
-src/caffe/layers/concat_layer.cpp \
-src/caffe/layers/multinomial_logistic_loss_layer.cpp \
-src/caffe/layers/tanh_layer.cpp \
-src/caffe/layers/loss_layer.cpp \
-src/caffe/layers/im2col_layer.cpp \
-src/caffe/layers/softmax_layer.cpp \
-src/caffe/layers/flatten_layer.cpp \
-src/caffe/layers/sigmoid_layer.cpp \
-src/caffe/layers/eltwise_layer.cpp \
-src/caffe/layers/bnll_layer.cpp \
-src/caffe/layers/hinge_loss_layer.cpp \
-src/caffe/layers/infogain_loss_layer.cpp \
-src/caffe/layers/softmax_loss_layer.cpp \
-src/caffe/layers/sigmoid_cross_entropy_loss_layer.cpp \
-src/caffe/layers/conv_layer.cpp \
-src/caffe/layers/lrn_layer.cpp \
-src/caffe/layers/inner_product_layer.cpp \
-src/caffe/layers/neuron_layer.cpp \
-src/caffe/layers/accuracy_layer.cpp \
-src/caffe/common.cpp \
-src/caffe/layer_factory.cpp
+CXX_SRC := caffe/src/caffe/syncedmem.cpp \
+caffe/src/caffe/util/upgrade_proto.cpp \
+caffe/src/caffe/util/math_functions.cpp \
+caffe/src/caffe/util/insert_splits.cpp \
+caffe/src/caffe/util/im2col.cpp \
+caffe/src/caffe/util/io.cpp \
+caffe/src/caffe/blob.cpp \
+caffe/src/caffe/net.cpp \
+caffe/src/caffe/layers/pooling_layer.cpp \
+caffe/src/caffe/layers/argmax_layer.cpp \
+caffe/src/caffe/layers/relu_layer.cpp \
+caffe/src/caffe/layers/split_layer.cpp \
+caffe/src/caffe/layers/euclidean_loss_layer.cpp \
+caffe/src/caffe/layers/power_layer.cpp \
+caffe/src/caffe/layers/slice_layer.cpp \
+caffe/src/caffe/layers/threshold_layer.cpp \
+caffe/src/caffe/layers/dropout_layer.cpp \
+caffe/src/caffe/layers/concat_layer.cpp \
+caffe/src/caffe/layers/multinomial_logistic_loss_layer.cpp \
+caffe/src/caffe/layers/tanh_layer.cpp \
+caffe/src/caffe/layers/loss_layer.cpp \
+caffe/src/caffe/layers/im2col_layer.cpp \
+caffe/src/caffe/layers/softmax_layer.cpp \
+caffe/src/caffe/layers/flatten_layer.cpp \
+caffe/src/caffe/layers/sigmoid_layer.cpp \
+caffe/src/caffe/layers/eltwise_layer.cpp \
+caffe/src/caffe/layers/bnll_layer.cpp \
+caffe/src/caffe/layers/hinge_loss_layer.cpp \
+caffe/src/caffe/layers/infogain_loss_layer.cpp \
+caffe/src/caffe/layers/softmax_loss_layer.cpp \
+caffe/src/caffe/layers/sigmoid_cross_entropy_loss_layer.cpp \
+caffe/src/caffe/layers/conv_layer.cpp \
+caffe/src/caffe/layers/lrn_layer.cpp \
+caffe/src/caffe/layers/inner_product_layer.cpp \
+caffe/src/caffe/layers/neuron_layer.cpp \
+caffe/src/caffe/layers/accuracy_layer.cpp \
+caffe/src/caffe/common.cpp \
+caffe/src/caffe/layer_factory.cpp
 
 LOCAL_SRC_FILES := $(CXX_SRC)
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/eigen3 $(LOCAL_PATH)/src $(LOCAL_PATH)/include
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../eigen3 $(LOCAL_PATH)/caffe/include \
+$(LOCAL_PATH)/../../boost/include/boost-1_55 \
+$(LOCAL_PATH)/../../opencv/jni/include
 
 LOCAL_STATIC_LIBRARIES += protobuf
+LOCAL_CPPFLAGS += -fexceptions -frtti -std=c++11 -DUSE_EIGEN -DCPU_ONLY
 
-# LOCAL_SHARED_LIBRARIES := libz libcutils libutils
-# LOCAL_LDLIBS := -lz
+# boost
+LOCAL_LDLIBS += -L$(LOCAL_PATH)/../../boost/lib/
+LOCAL_LDLIBS += -lboost_random-gcc-mt-1_55 -lboost_math_tr1-gcc-mt-1_55
+# opencv
+# LOCAL_LDLIBS += -L$(LOCAL_PATH)/../../opencv/libs/$(TARGET_ARCH_ABI)
+# LOCAL_LDLIBS += -lopencv_core -lopencv_highgui -lopencv_imgproc
 
-# Define the header files to be copied
-#LOCAL_COPY_HEADERS := \
-#    src/google/protobuf/stubs/once.h \
-#    src/google/protobuf/stubs/common.h \
-#    src/google/protobuf/io/coded_stream.h \
-#    src/google/protobuf/generated_message_util.h \
-#    src/google/protobuf/repeated_field.h \
-#    src/google/protobuf/extension_set.h \
-#    src/google/protobuf/wire_format_lite_inl.h
-#
-#LOCAL_COPY_HEADERS_TO := $(LOCAL_MODULE)
-LOCAL_CPPFLAGS   += -fexceptions -frtti -std=c++11 -DUSE_EIGEN -DCPU_ONLY
+LOCAL_ARM_MODE  := arm
 
 include $(BUILD_STATIC_LIBRARY)
