9d85852640fc354295b0b82edf8b04510b6e8e1a
sh1r0 <sh1r0@users.noreply.github.com>
2016-01-14 00:21:07 +0800

    Remove hard-assigned ANDROID_TOOLCHAIN_NAME

--- scripts/build_caffe.sh
+++ scripts/build_caffe.sh
@@ -49,7 +49,6 @@ cmake -DCMAKE_TOOLCHAIN_FILE="${WD}/android-cmake/android.toolchain.cmake" \
       -DCMAKE_BUILD_TYPE=Release \
       -DANDROID_ABI="${ANDROID_ABI}" \
       -DANDROID_NATIVE_API_LEVEL=21 \
-      -DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-4.9 \
       -DANDROID_USE_OPENMP=ON \
       -DADDITIONAL_FIND_PATH="${ANDROID_LIB_ROOT}" \
       -DBUILD_python=OFF \
