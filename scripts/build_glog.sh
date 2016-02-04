44d8a128047a193c6d16fab66bed8af137444912
sh1r0 <sh1r0@users.noreply.github.com>
2016-02-04 10:32:58 +0800

    Add support for glog (no logcat redirection yet)

---
+++ scripts/build_glog.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+set -e
+
+if [ -z "$NDK_ROOT" ] && [ "$#" -eq 0 ]; then
+    echo 'Either $NDK_ROOT should be set or provided as argument'
+    echo "e.g., 'export NDK_ROOT=/path/to/ndk' or"
+    echo "      '${0} /path/to/ndk'"
+    exit 1
+else
+    NDK_ROOT="${1:-${NDK_ROOT}}"
+fi
+
+ANDROID_ABI=${ANDROID_ABI:-"armeabi-v7a with NEON"}
+WD=$(readlink -f "`dirname $0`/..")
+GLOG_ROOT=${WD}/glog
+BUILD_DIR=${GLOG_ROOT}/build
+ANDROID_LIB_ROOT=${WD}/android_lib
+GFLAGS_HOME=${ANDROID_LIB_ROOT}/gflags
+N_JOBS=${N_JOBS:-4}
+
+rm -rf "${BUILD_DIR}"
+mkdir -p "${BUILD_DIR}"
+cd "${BUILD_DIR}"
+
+cmake -DCMAKE_TOOLCHAIN_FILE="${WD}/android-cmake/android.toolchain.cmake" \
+      -DANDROID_NDK="${NDK_ROOT}" \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DANDROID_ABI="${ANDROID_ABI}" \
+      -DANDROID_NATIVE_API_LEVEL=21 \
+      -DGFLAGS_INCLUDE_DIR="${GFLAGS_HOME}/include" \
+      -DGFLAGS_LIBRARY="${GFLAGS_HOME}/lib/libgflags.a" \
+      -DCMAKE_INSTALL_PREFIX="${ANDROID_LIB_ROOT}/glog" \
+      ..
+
+make -j${N_JOBS}
+rm -rf "${ANDROID_LIB_ROOT}/glog"
+make install/strip
+
+cd "${WD}"
