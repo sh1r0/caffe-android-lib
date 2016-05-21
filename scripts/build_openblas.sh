b679b9a5d0651bf890830f22f35f0175bd3fa845
sh1r0 <sh1r0@users.noreply.github.com>
2016-05-21 21:39:08 +0800

    Updated OpenBLAS to v0.2.18; revised the build script

--- scripts/build_openblas.sh
+++ scripts/build_openblas.sh
@@ -34,39 +34,48 @@ N_JOBS=${N_JOBS:-4}
 
 cd "${OPENBLAS_ROOT}"
 
-make clean
 if [ "${ANDROID_ABI}" = "armeabi-v7a-hard-softfp with NEON" ]; then
-    TOOLCHAIN_DIR=$NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/${OS}-${BIT}/bin
-    make -j${N_JOBS} \
-         CC="$TOOLCHAIN_DIR/arm-linux-androideabi-gcc --sysroot=$NDK_ROOT/platforms/android-21/arch-arm" \
-         CROSS_SUFFIX=$TOOLCHAIN_DIR/arm-linux-androideabi- \
-         HOSTCC=gcc NO_LAPACK=1 TARGET=ARMV7 \
-         USE_THREAD=1 NUM_THREADS=8 USE_OPENMP=1
+    CROSS_SUFFIX=$NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/${OS}-${BIT}/bin/arm-linux-androideabi-
+    SYSROOT=$NDK_ROOT/platforms/android-21/arch-arm
+    NO_LAPACK=${NO_LAPACK:-1}
+	TARGET=ARMV7
+	BINARY=32
 elif [ "${ANDROID_ABI}" = "arm64-v8a"  ]; then
-    TOOLCHAIN_DIR=$NDK_ROOT/toolchains/aarch64-linux-android-4.9/prebuilt/${OS}-${BIT}/bin
-    make -j${N_JOBS} \
-         CC="$TOOLCHAIN_DIR/aarch64-linux-android-gcc --sysroot=$NDK_ROOT/platforms/android-21/arch-arm64" \
-         CROSS_SUFFIX=$TOOLCHAIN_DIR/aarch64-linux-android- \
-         HOSTCC=gcc NO_LAPACK=1 TARGET=ARMV8 \
-         USE_THREAD=1 NUM_THREADS=8 USE_OPENMP=1
+    CROSS_SUFFIX=$NDK_ROOT/toolchains/aarch64-linux-android-4.9/prebuilt/${OS}-${BIT}/bin/aarch64-linux-android-
+    SYSROOT=$NDK_ROOT/platforms/android-21/arch-arm64
+    NO_LAPACK=${NO_LAPACK:-1}
+	TARGET=ARMV8
+	BINARY=64
 elif [ "${ANDROID_ABI}" = "armeabi"  ]; then
-    TOOLCHAIN_DIR=$NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/${OS}-${BIT}/bin
-    make -j${N_JOBS} \
-         CC="$TOOLCHAIN_DIR/arm-linux-androideabi-gcc --sysroot=$NDK_ROOT/platforms/android-21/arch-arm" \
-         CROSS_SUFFIX=$TOOLCHAIN_DIR/arm-linux-androideabi- \
-         HOSTCC=gcc NO_LAPACK=1 TARGET=ARMV5 \
-         USE_THREAD=1 NUM_THREADS=8 USE_OPENMP=1
+    CROSS_SUFFIX=$NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/${OS}-${BIT}/bin/arm-linux-androideabi-
+    SYSROOT=$NDK_ROOT/platforms/android-21/arch-arm
+    NO_LAPACK=1
+	TARGET=ARMV5
+	BINARY=32
+elif [ "${ANDROID_ABI}" = "x86"  ]; then
+    CROSS_SUFFIX=$NDK_ROOT/toolchains/x86-4.9/prebuilt/${OS}-${BIT}/bin/i686-linux-android-
+    SYSROOT=$NDK_ROOT/platforms/android-21/arch-x86
+    NO_LAPACK=1
+	TARGET=ATOM
+	BINARY=32
 elif [ "${ANDROID_ABI}" = "x86_64"  ]; then
-    TOOLCHAIN_DIR=$NDK_ROOT/toolchains/x86_64-4.9/prebuilt/${OS}-${BIT}/bin
-    make -j${N_JOBS} \
-         CC="$TOOLCHAIN_DIR/x86_64-linux-android-gcc --sysroot=$NDK_ROOT/platforms/android-21/arch-x86_64" \
-         CROSS_SUFFIX=$TOOLCHAIN_DIR/x86_64-linux-android- \
-         HOSTCC=gcc NO_LAPACK=1 TARGET=ATOM BINARY=64 \
-         USE_THREAD=1 NUM_THREADS=8 USE_OPENMP=1
+    CROSS_SUFFIX=$NDK_ROOT/toolchains/x86_64-4.9/prebuilt/${OS}-${BIT}/bin/x86_64-linux-android-
+    SYSROOT=$NDK_ROOT/platforms/android-21/arch-x86_64
+    NO_LAPACK=1
+	TARGET=ATOM
+	BINARY=64
 else
     echo "Error: not support OpenBLAS for ABI: ${ANDROID_ABI}"
     exit 1
 fi
+
+make clean
+make -j${N_JOBS} \
+     CC="${CROSS_SUFFIX}gcc --sysroot=$SYSROOT" \
+     FC="${CROSS_SUFFIX}gfortran --sysroot=$SYSROOT" \
+     CROSS_SUFFIX=$CROSS_SUFFIX \
+     HOSTCC=gcc USE_THREAD=1 NUM_THREADS=8 USE_OPENMP=1 \
+     NO_LAPACK=$NO_LAPACK TARGET=$TARGET BINARY=$BINARY
 
 rm -rf "$INSTALL_DIR/openblas"
 make PREFIX="$INSTALL_DIR/openblas" install
